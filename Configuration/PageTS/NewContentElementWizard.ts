d191834438b3f4853dcabe50be8b438c9413760e
SvenJuergens <sj@nordsonne.de>
2017-01-27 17:11:56 +0100

    [BUGFIX] Wizard name and description path

--- Configuration/PageTS/NewContentElementWizard.ts
+++ Configuration/PageTS/NewContentElementWizard.ts
@@ -3,9 +3,9 @@ mod.wizards {
         plugins {
             elements {
                 plugins_tx_jpcarousel_pi1 {
-                    icon = EXT:jpcarousel/pi/ce_wiz.gif
-                    title = LLL:EXT:jpcarousel/locallang.xml:pi_title
-                    description = LLL:EXT:jpcarousel/locallang.xml:pi_plus_wiz_description
+                    icon = EXT:jpcarousel/pi1/ce_wiz.gif
+                    title = LLL:EXT:jpcarousel/locallang.xml:pi1_title
+                    description = LLL:EXT:jpcarousel/locallang.xml:pi1_plus_wiz_description
                     tt_content_defValues {
                         CType = list
                         list_type = jpcarousel_pi1
